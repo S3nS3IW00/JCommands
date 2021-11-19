128f6fec9bc6b098580dd43c1d282eaeed4e5bdc
S3nS3IW00 <danielgellert5@gmail.com>
2021-11-19 13:11:40 +0100

    Change value type

--- src/main/java/me/s3ns3iw00/jcommands/argument/converter/ArgumentResultConverter.java
+++ src/main/java/me/s3ns3iw00/jcommands/argument/converter/ArgumentResultConverter.java
@@ -29,6 +29,6 @@ public interface ArgumentResultConverter {
      * @param value the raw value
      * @return the converted object
      */
-    Object convertTo(String value);
+    Object convertTo(Object value);
 
 }
