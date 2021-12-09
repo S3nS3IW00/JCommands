06f2228e13e5f39fa847940289a023bab4baf96a
S3nS3IW00 <danielgellert5@gmail.com>
2021-12-09 11:14:13 +0100

    Create helper interface to identify classes

---
+++ src/main/java/me/s3ns3iw00/jcommands/argument/NestedArgument.java
@@ -0,0 +1,9 @@
+package me.s3ns3iw00.jcommands.argument;
+
+/**
+ * A helper interface
+ * It helps to identify arguments that can be contained by {@link me.s3ns3iw00.jcommands.argument.type.ConstantArgument}
+ * Arguments that implements this interface can be nested
+ */
+public interface NestedArgument extends Argument {
+}
