a2cda8c0dd71e80c57919d8f8c5f5a8ba355d0b6
S3nS3IW00 <danielgellert5@gmail.com>
2021-12-09 10:48:45 +0100

    Create GroupArgument class that only can contain ConstantArgument for be able to group SUB_COMMAND options

---
+++ src/main/java/me/s3ns3iw00/jcommands/argument/type/GroupArgument.java
@@ -0,0 +1,62 @@
+package me.s3ns3iw00.jcommands.argument.type;
+
+import me.s3ns3iw00.jcommands.argument.Argument;
+import org.javacord.api.interaction.SlashCommandOption;
+import org.javacord.api.interaction.SlashCommandOptionType;
+
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+/**
+ * Represents an argument with {@code SUB_COMMAND_GROUP} type that only can contain {@link ConstantArgument}
+ * This is for grouping {@code SUB_COMMAND} options because {@code SUB_COMMANDS} cannot be nested
+ */
+public class GroupArgument implements Argument {
+
+    private final String name, description;
+    private final LinkedList<ConstantArgument> arguments = new LinkedList<>();
+
+    public GroupArgument(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public Object getValue() {
+        return name;
+    }
+
+    @Override
+    public Class<?> getResultType() {
+        return String.class;
+    }
+
+    @Override
+    public SlashCommandOption getCommandOption() {
+        return SlashCommandOption.createWithOptions(SlashCommandOptionType.SUB_COMMAND_GROUP, getName(), getDescription(), getArguments().stream().map(Argument::getCommandOption).collect(Collectors.toList()));
+    }
+
+    /**
+     * Adds a {@link ConstantArgument} to the argument
+     *
+     * @param argument the argument
+     */
+    public void addArgument(ConstantArgument argument) {
+        arguments.add(argument);
+    }
+
+    public LinkedList<ConstantArgument> getArguments() {
+        return arguments;
+    }
+
+}
