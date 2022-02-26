625a5dc29546cf7d9b5c0bcbf59bd297fb34768d
S3nS3IW00 <danielgellert5@gmail.com>
2022-02-26 23:04:25 +0100

    Create interface for implementing optionality

---
+++ src/main/java/me/s3ns3iw00/jcommands/argument/util/Optionality.java
@@ -0,0 +1,10 @@
+package me.s3ns3iw00.jcommands.argument.util;
+
+/**
+ * Identifies whether an argument can be optional or not
+ */
+public interface Optionality {
+
+    boolean isOptional();
+
+}
