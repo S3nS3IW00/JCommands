2b9b8662b086e70a2192f18a44301e4925fafc00
S3nS3IW00 <danielgellert5@gmail.com>
2023-03-31 10:05:37 +0200

    Update converter

--- src/test/java/me/s3ns3iw00/jcommands/person/converter/DateConverter.java
+++ src/test/java/me/s3ns3iw00/jcommands/person/converter/DateConverter.java
@@ -4,22 +4,23 @@ import me.s3ns3iw00.jcommands.argument.converter.ArgumentResultConverter;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * An {@link ArgumentResultConverter} that converts {@link String} value formatted by {@link DateConverter#DATE_FORMAT} into {@link java.util.Date}
  */
-public class DateConverter implements ArgumentResultConverter {
+public class DateConverter implements ArgumentResultConverter<String, Date> {
 
     // The date format
     public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     @Override
-    public Object convertTo(Object value) {
+    public Date convertTo(String value) {
         /* Don't need to check anything, since
            type and formatting is already validated by the argument itself
          */
         try {
-            return DATE_FORMAT.parse(String.valueOf(value));
+            return DATE_FORMAT.parse(value);
         } catch (ParseException e) {
             return null;
         }
