92ddebbeb156f9bf13490ba54dbdbe769b2ac03d
S3nS3IW00 <danielgellert5@gmail.com>
2024-01-06 20:05:43 +0100

    Update example

--- src/test/java/me/s3ns3iw00/jcommands/person/PersonCommand.java
+++ src/test/java/me/s3ns3iw00/jcommands/person/PersonCommand.java
@@ -1,11 +1,8 @@
 package me.s3ns3iw00.jcommands.person;
 
-import me.s3ns3iw00.jcommands.CommandResponder;
-import me.s3ns3iw00.jcommands.argument.ArgumentResult;
 import me.s3ns3iw00.jcommands.argument.concatenation.type.StringConcatenator;
 import me.s3ns3iw00.jcommands.argument.concatenation.type.TypeConcatenator;
 import me.s3ns3iw00.jcommands.event.listener.CommandActionEventListener;
-import me.s3ns3iw00.jcommands.event.type.CommandActionEvent;
 import me.s3ns3iw00.jcommands.person.argument.AddArgument;
 import me.s3ns3iw00.jcommands.person.model.Person;
 import me.s3ns3iw00.jcommands.type.ServerCommand;
@@ -13,7 +10,7 @@ import me.s3ns3iw00.jcommands.type.ServerCommand;
 /**
  * A {@link me.s3ns3iw00.jcommands.type.ServerCommand} to manage people, and a {@link CommandActionEventListener} to be able to listen for its usage
  */
-public class PersonCommand extends ServerCommand implements CommandActionEventListener {
+public class PersonCommand extends ServerCommand {
 
     /**
      * Default constructor
@@ -21,7 +18,6 @@ public class PersonCommand extends ServerCommand implements CommandActionEventLi
     public PersonCommand() {
         // Call super class's constructor with a name and a description
         super("person", "A command to manage people");
-        setOnAction(this); // set this class as action listener
 
         // Instantiate the sub command and adds it to the command
         AddArgument addArgument = new AddArgument();
@@ -36,26 +32,6 @@ public class PersonCommand extends ServerCommand implements CommandActionEventLi
         // This could be wrong because there is no constructor that takes two String and a Date type of parameter but the first two parameters will be replaced by the result of the concatenator above,
         // so it will be a String and a Date type of parameter and there is a constructor that takes these parameters
         addConcatenator(new TypeConcatenator<>(Person.class), addArgument.getFirstNameArgument(), addArgument.getLastNameArgument(), addArgument.getDateOfBirthArgument());
-    }
-
-    /**
-     * The action listener
-     *
-     * @param event the event
-     */
-    @Override
-    public void onAction(CommandActionEvent event) {
-        ArgumentResult[] args = event.getArguments();
-        CommandResponder responder = event.getResponder();
-
-        String action = args[0].get();
-        if (action.equalsIgnoreCase("add")) {
-            Person person = args[1].get();
-
-            responder.respondNow()
-                    .setContent(person.getFullName() + " (" + person.getAge() + ")")
-                    .respond();
-        }
     }
 
 }
--- src/test/java/me/s3ns3iw00/jcommands/person/argument/AddArgument.java
+++ src/test/java/me/s3ns3iw00/jcommands/person/argument/AddArgument.java
@@ -1,16 +1,21 @@
 package me.s3ns3iw00.jcommands.person.argument;
 
+import me.s3ns3iw00.jcommands.CommandResponder;
+import me.s3ns3iw00.jcommands.argument.ArgumentResult;
 import me.s3ns3iw00.jcommands.argument.type.ConstantArgument;
 import me.s3ns3iw00.jcommands.argument.type.StringArgument;
 import me.s3ns3iw00.jcommands.argument.validator.type.RegexPredicate;
+import me.s3ns3iw00.jcommands.event.listener.CommandActionEventListener;
+import me.s3ns3iw00.jcommands.event.type.CommandActionEvent;
 import me.s3ns3iw00.jcommands.person.converter.DateConverter;
+import me.s3ns3iw00.jcommands.person.model.Person;
 
 import java.util.Date;
 
 /**
  * A {@link ConstantArgument} that gathers data for creating new {@link me.s3ns3iw00.jcommands.person.model.Person}
  */
-public class AddArgument extends ConstantArgument {
+public class AddArgument extends ConstantArgument implements CommandActionEventListener {
 
     // The inputs of this argument
     private final StringArgument<String> firstNameArgument, lastNameArgument;
@@ -22,6 +27,7 @@ public class AddArgument extends ConstantArgument {
     public AddArgument() {
         // Call super class's constructor with a name and a description
         super("add", "Adds a person to people");
+        setOnAction(this); // set this class as action listener
 
         // First name of the person with a length validation
         firstNameArgument = new StringArgument<>("firstname", "The first name of the person, minimum 8, maximum 16 character", String.class);
@@ -51,6 +57,23 @@ public class AddArgument extends ConstantArgument {
 
         // Add all the arguments to the argument
         addArgument(firstNameArgument, lastNameArgument, dateOfBirthArgument);
+    }
+
+    /**
+     * The action listener
+     *
+     * @param event the event
+     */
+    @Override
+    public void onAction(CommandActionEvent event) {
+        ArgumentResult[] args = event.getArguments();
+        CommandResponder responder = event.getResponder();
+
+        Person person = args[0].get();
+
+        responder.respondNow()
+                .setContent(person.getFullName() + " (" + person.getAge() + ")")
+                .respond();
     }
 
     public StringArgument<String> getFirstNameArgument() {
